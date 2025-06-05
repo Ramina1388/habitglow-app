cbc92b343a7f466532246aff4780c133ab4a6678
Marina Yakovleva <marinayakovleva@Marinas-MacBook-Pro.local>
2025-06-05 02:20:00 -0300

    Build after full reinstall, ready to deploy

--- app/api/farcaster-user/route.js
+++ app/api/farcaster-user/route.js
@@ -5,9 +5,9 @@ const config = new Configuration({
   apiKey: process.env.NEYNAR_API_KEY,
   baseOptions: {
     headers: {
-      "x-neynar-experimental": true,
-    },
-  },
+      "x-neynar-experimental": true
+    }
+  }
 });
 
 const client = new NeynarAPIClient(config);
